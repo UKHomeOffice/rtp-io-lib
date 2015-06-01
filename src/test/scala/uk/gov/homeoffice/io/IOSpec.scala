a90900e495061b68b7b6c3aa92b0065475ed80df
David Ainslie <dainslie@gmail.com>
2015-06-01 23:22:38 +0100

    Renamed example resource files

--- src/test/scala/uk/gov/homeoffice/io/IOSpec.scala
+++ src/test/scala/uk/gov/homeoffice/io/IOSpec.scala
@@ -8,17 +8,17 @@ import org.specs2.mutable.Specification
 class IOSpec extends Specification with IO {
   "Classpath resource" should {
     "be found from a given string representing the path" in {
-      fromClasspath(path("/io-test-1.txt")) must beLike {
-        case Success(u: URL) => u.getFile must endWith(path("test-classes/io-test-1.txt"))
+      fromClasspath(path("/test-1.txt")) must beLike {
+        case Success(u: URL) => u.getFile must endWith(path("test-classes/test-1.txt"))
       }
     }
 
     "not be found from a given string representing the path" in {
-      fromClasspath(path("/io-test-0.txt")) must beFailedTry.withThrowable[IOException]
+      fromClasspath(path("/blah.txt")) must beFailedTry.withThrowable[IOException]
     }
 
     "give its content" in {
-      fromClasspath(path("/io-test-1.txt")) flatMap urlContentToString must beSuccessfulTry("Hello World!")
+      fromClasspath(path("/test-1.txt")) flatMap urlContentToString must beSuccessfulTry("Hello World!")
     }
   }
 }
--- src/test/scala/uk/gov/homeoffice/json/JsonSpec.scala
+++ src/test/scala/uk/gov/homeoffice/json/JsonSpec.scala
@@ -7,23 +7,23 @@ import org.specs2.mutable.Specification
 class JsonSpec extends Specification with Json {
   "Classpath JSON resource" should {
     "give its content" in {
-      jsonFromClasspath(path("/io-test-2.json")) must beSuccessfulTry { JObject("hello" -> JString("world!")) }
+      jsonFromClasspath(path("/test-2.json")) must beSuccessfulTry { JObject("hello" -> JString("world!")) }
     }
   }
 
   "URL JSON resource" should {
     "give its content" in {
-      fromClasspath(path("/io-test-2.json")) flatMap jsonFromUrlContent must beSuccessfulTry { JObject("hello" -> JString("world!")) }
+      fromClasspath(path("/test-2.json")) flatMap jsonFromUrlContent must beSuccessfulTry { JObject("hello" -> JString("world!")) }
     }
   }
 
   "File JSON resource" should {
     "give its content" in {
-      jsonFromFilepath(path("src/test/resources/io-test-2.json")) must beSuccessfulTry { JObject("hello" -> JString("world!")) }
+      jsonFromFilepath(path("src/test/resources/test-2.json")) must beSuccessfulTry { JObject("hello" -> JString("world!")) }
     }
 
     "not be found" in {
-      jsonFromFilepath(path("src/test/resources/io-test-0.json")) must beFailedTry.withThrowable[FileNotFoundException]
+      jsonFromFilepath(path("src/test/resources/blah.json")) must beFailedTry.withThrowable[FileNotFoundException]
     }
   }
 }
