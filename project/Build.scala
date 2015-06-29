1dcf61ce5340f2c2fb7a7dc846b263412c07887b
David Ainslie <dainslie@gmail.com>
2015-06-29 17:10:01 +0100

    Updated config factory lib

--- project/Build.scala
+++ project/Build.scala
@@ -28,7 +28,7 @@ object Build extends Build {
         "Kamon Repository" at "http://repo.kamon.io"),
       libraryDependencies ++= Seq(
         "org.slf4j" % "slf4j-api" % "1.7.12" withSources(),
-        "com.typesafe" % "config" % "1.2.1" withSources(),
+        "com.typesafe" % "config" % "1.3.0" withSources(),
         "org.json4s" %% "json4s-native" % "3.2.11" withSources(),
         "org.json4s" %% "json4s-ext" % "3.2.11" withSources(),
         "com.github.fge" % "json-schema-validator" % "2.2.6" withSources(),
