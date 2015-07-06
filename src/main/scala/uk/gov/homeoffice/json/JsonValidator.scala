8988f55f1ddcaad44273d23b0166848414908dcc
David Ainslie <dainslie@gmail.com>
2015-07-06 10:24:49 +0100

    NoJsonValidator to allow schemaless use of JSON

--- src/main/scala/uk/gov/homeoffice/json/JsonValidator.scala
+++ src/main/scala/uk/gov/homeoffice/json/JsonValidator.scala
@@ -1,10 +1,26 @@
 package uk.gov.homeoffice.json
 
 import org.json4s._
+import org.json4s.native.JsonMethods._
 import org.scalactic.Or
 
 trait JsonValidator {
   def jsonSchema: JsonSchema
 
   def validate(json: JValue): JValue Or JsonError = jsonSchema validate json
+}
+
+/**
+ * The use of this JSON Validator is not recommended.
+ * However, there may be a situation where you do not have a JSON schema but still want to use the JSON functionality provided by this library e.g. for tests.
+ */
+trait NoJsonValidator extends JsonValidator {
+  val jsonSchema = JsonSchema(parse("""
+  {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "id": "http://www.gov.uk/no-json-validator",
+    "type": "object",
+    "properties": {
+    }
+  }"""))
 }
