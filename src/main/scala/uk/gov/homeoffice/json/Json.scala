acde9b514cd387f6888005b92fc4c9c47a3e6a27
David Ainslie <dainslie@gmail.com>
2015-07-09 16:19:21 +0100

    Updated for changes to akka-lib

--- src/main/scala/uk/gov/homeoffice/json/Json.scala
+++ src/main/scala/uk/gov/homeoffice/json/Json.scala
@@ -1,11 +1,14 @@
 package uk.gov.homeoffice.json
 
+import java.io.IOException
 import java.net.URL
 import scala.io.Source
 import scala.util.Try
+import scala.util.control.NonFatal
 import org.json4s.JValue
 import org.json4s.JsonDSL._
 import org.json4s.native.JsonMethods._
+import org.scalactic.{Bad, Or}
 import uk.gov.homeoffice.io.IO
 
 object Json extends Json
@@ -19,6 +22,12 @@ trait Json extends IO {
 
   val jsonFromFilepath: String => Try[JValue] =
     filepath => Try { parse(Source.fromFile(filepath).getLines().mkString) }
+
+  val error = (json: JValue) => PartialFunction[Throwable, JValue Or JsonError] {
+    case e: IOException => Bad(JsonError(json, e.getMessage, Some(e), fatalException = true))
+    case NonFatal(n) => Bad(JsonError(json, n.getMessage, Some(n)))
+    case t => Bad(JsonError(json, t.getMessage, Some(t), fatalException = true))
+  }
 
   implicit class JFieldOps(jfield: (String, JValue)) {
     def merge(json: JValue) = (jfield: JValue) merge json
--- src/test/scala/uk/gov/homeoffice/json/JsonSpec.scala
+++ src/test/scala/uk/gov/homeoffice/json/JsonSpec.scala
@@ -1,7 +1,10 @@
 package uk.gov.homeoffice.json
 
-import java.io.FileNotFoundException
-import org.json4s.JsonAST.{JObject, JString}
+import java.io.{IOException, FileNotFoundException}
+import scala.util.control.NonFatal
+import org.json4s.{JValue, JObject, JString}
+import org.json4s.JsonDSL._
+import org.scalactic.{Good, Or, Bad}
 import org.specs2.mutable.Specification
 
 class JsonSpec extends Specification with Json {
@@ -26,4 +29,72 @@ class JsonSpec extends Specification with Json {
       jsonFromFilepath(path("src/test/resources/blah.json")) must beFailedTry.withThrowable[FileNotFoundException]
     }
   }
+
+  /*"JSON" should {
+    "give non fatal JSON error" in {
+      val json = "hello" -> "world"
+
+      error(json)(new IllegalArgumentException) must beLike {
+        case Bad(j) => j.fatalException must beFalse
+      }
+    }
+
+    "give fatal JSON error" in {
+      def errorX(pfs: (JValue => PartialFunction[Throwable, JValue Or JsonError])*)(json: JValue) = {
+        pfs.foldRight {
+          PartialFunction[Throwable, JValue Or JsonError] {
+            case e: IOException => Bad(JsonError(json, e.getMessage, Some(e), fatalException = true))
+            case NonFatal(n) => Bad(JsonError(json, n.getMessage, Some(n)))
+            case t => Bad(JsonError(json, t.getMessage, Some(t), fatalException = true))
+          }
+        } { (pf, acc) => pf(json).applyOrElse(acc) }
+      }
+
+      val v = errorX("asdf" -> "asdf")()
+
+      v
+
+      def error(pfs: PartialFunction[Throwable, JValue Or JsonError]*) = (json: JValue) => {
+        pfs.foldRight {
+          PartialFunction[Throwable, JValue Or JsonError] {
+            case e: IOException => Bad(JsonError(json, e.getMessage, Some(e), fatalException = true))
+            case NonFatal(n) => Bad(JsonError(json, n.getMessage, Some(n)))
+            case t => Bad(JsonError(json, t.getMessage, Some(t), fatalException = true))
+          }
+        } { (pf, acc) => pf.orElse(acc) }
+      }
+
+
+      //val json: JObject = "hello" -> "world"
+
+      error(PartialFunction[Throwable, JValue Or JsonError] {
+        case i: IllegalArgumentException => Bad(JsonError(json, i.getMessage, Some(i), fatalException = true))
+      })("hello" -> "world")
+
+
+      val fatal = (json: JValue) => PartialFunction[Throwable, JValue Or JsonError] {
+        case i: IllegalArgumentException => Bad(JsonError(json, i.getMessage, Some(i), fatalException = true))
+      }
+
+
+
+      error(json, fatal(json)) must beLike {
+        case Bad(j) => j.fatalException must beTrue
+      }
+
+
+      /*val json: JObject = "hello" -> "world"
+
+      val fatal = (json: JValue) => PartialFunction[Throwable, JValue Or JsonError] {
+        case i: IllegalArgumentException => Bad(JsonError(json, i.getMessage, Some(i), fatalException = true))
+      }
+
+      val v = fatal(json).orElse(error(json))
+      v
+
+      fatal(json).  .applyOrElse(error(json)) must beLike {
+        case Bad(j) => j.fatalException must beTrue
+      }*/
+    }
+  }*/
 }
