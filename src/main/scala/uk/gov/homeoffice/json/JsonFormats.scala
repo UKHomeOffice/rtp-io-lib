6c2589ca22498a5ce58a14f93b0dbe9872e9da8c
David Ainslie <dainslie@gmail.com>
2015-07-10 18:30:15 +0100

    Added object

--- src/main/scala/uk/gov/homeoffice/json/JsonFormats.scala
+++ src/main/scala/uk/gov/homeoffice/json/JsonFormats.scala
@@ -4,6 +4,8 @@ import org.json4s.DefaultFormats
 import org.json4s.ext.JodaTimeSerializers
 import org.json4s.mongo.ObjectIdSerializer
 
+object JsonFormats extends JsonFormats
+
 trait JsonFormats {
   implicit val json4sFormats = DefaultFormats + new ObjectIdSerializer ++ JodaTimeSerializers.all
 }
