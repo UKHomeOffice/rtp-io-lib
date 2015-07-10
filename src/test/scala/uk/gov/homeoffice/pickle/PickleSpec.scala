147428d244da235acd48dc7df8a0621853b8356d
David Ainslie <dainslie@gmail.com>
2015-07-10 13:42:56 +0100

    Pickling

---
+++ src/test/scala/uk/gov/homeoffice/pickle/PickleSpec.scala
@@ -0,0 +1,26 @@
+package uk.gov.homeoffice.pickle
+
+import scala.pickling.Defaults._
+import scala.pickling.json._
+import org.json4s.native.JsonMethods._
+import org.specs2.mutable.Specification
+import uk.gov.homeoffice.json.{JsonFormats => Json4sFormats}
+
+class PickleSpec extends Specification with Json4sFormats {
+  "Class" should {
+    class Person(val name: String, val age: Int)
+
+    "be pickled" in {
+      val person = new Person("Batman", 20)
+      val pickledPerson = person.pickle
+      val json = parse(pickledPerson.value)
+
+      (json \ "name").extract[String] mustEqual "Batman"
+
+      val unpickledPerson = pickledPerson.unpickle[Person]
+
+      unpickledPerson.name mustEqual person.name
+      unpickledPerson.age mustEqual person.age
+    }
+  }
+}
