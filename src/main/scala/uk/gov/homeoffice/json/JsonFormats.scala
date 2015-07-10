cd638bd394cb8ea905ebaaf3a33ab8705f698662
David Ainslie <dainslie@gmail.com>
2015-07-10 16:12:47 +0100

    Mongo Object ID format

--- src/main/scala/uk/gov/homeoffice/json/JsonFormats.scala
+++ src/main/scala/uk/gov/homeoffice/json/JsonFormats.scala
@@ -2,7 +2,8 @@ package uk.gov.homeoffice.json
 
 import org.json4s.DefaultFormats
 import org.json4s.ext.JodaTimeSerializers
+import org.json4s.mongo.ObjectIdSerializer
 
 trait JsonFormats {
-  implicit val json4sFormats = DefaultFormats ++ JodaTimeSerializers.all
+  implicit val json4sFormats = DefaultFormats + new ObjectIdSerializer ++ JodaTimeSerializers.all
 }
