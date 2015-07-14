901b3bba21fa697244c31c0a20df3f3d3d61ea7e
IrfanAnsari <irfanaansari@gmail.com>
2015-07-14 11:32:59 +0100

    Renamed a variable to reflect its Throwable not an Exception

--- src/main/scala/uk/gov/homeoffice/json/JsonError.scala
+++ src/main/scala/uk/gov/homeoffice/json/JsonError.scala
@@ -3,4 +3,4 @@ package uk.gov.homeoffice.json
 import org.json4s.JValue
 import org.json4s.JsonAST.JNothing
 
-case class JsonError(json: JValue = JNothing, error: String, exception: Option[Throwable] = None)
+case class JsonError(json: JValue = JNothing, error: String, throwable: Option[Throwable] = None)
