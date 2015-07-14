4303060e50ca66c5a5daacedd2724131e6852873
David Ainslie <dainslie@gmail.com>
2015-07-14 11:51:54 +0100

    Bad JSON only relevant to Rabbit

--- src/main/scala/uk/gov/homeoffice/json/Json.scala
+++ src/main/scala/uk/gov/homeoffice/json/Json.scala
@@ -1,14 +1,11 @@
 package uk.gov.homeoffice.json
 
-import java.io.IOException
 import java.net.URL
 import scala.io.Source
 import scala.util.Try
-import scala.util.control.NonFatal
 import org.json4s.JValue
 import org.json4s.JsonDSL._
 import org.json4s.native.JsonMethods._
-import org.scalactic.{Bad, Or}
 import uk.gov.homeoffice.io.IO
 
 object Json extends Json
@@ -22,17 +19,6 @@ trait Json extends IO {
 
   val jsonFromFilepath: String => Try[JValue] =
     filepath => Try { parse(Source.fromFile(filepath).getLines().mkString) }
-
-  // TODO Scaladoc
-  /*def bad[G](json: JValue, pf: JValue => PartialFunction[Throwable, G Or JsonError] = (j: JValue) => PartialFunction.empty[Throwable, G Or JsonError]) = {
-    val defaultPF = PartialFunction[Throwable, G Or JsonError] {
-      case e: IOException => Bad(JsonError(json, e.getMessage, Some(e), fatalException = true))
-      case NonFatal(n) => Bad(JsonError(json, n.getMessage, Some(n)))
-      case t => Bad(JsonError(json, t.getMessage, Some(t), fatalException = true))
-    }
-
-    pf(json) orElse defaultPF
-  }*/
 
   implicit class JFieldOps(jfield: (String, JValue)) {
     def merge(json: JValue) = (jfield: JValue) merge json
