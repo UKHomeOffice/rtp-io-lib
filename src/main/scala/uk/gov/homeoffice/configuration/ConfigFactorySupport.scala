696b819b5e41a03c7350fefde4504433d98dbca6
David Ainslie <dainslie@gmail.com>
2015-07-21 16:15:25 +0100

    Configuration extension.

--- src/main/scala/uk/gov/homeoffice/configuration/ConfigFactorySupport.scala
+++ src/main/scala/uk/gov/homeoffice/configuration/ConfigFactorySupport.scala
@@ -1,21 +1,36 @@
 package uk.gov.homeoffice.configuration
 
-import java.time.Duration
+import java.time.{Duration => JavaDuration}
 import java.util.concurrent.TimeUnit
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration._
+import scala.util.Try
 import com.typesafe.config.{Config, ConfigValue, ConfigValueFactory}
 
 trait ConfigFactorySupport {
   implicit val anyToConfigValue: Any => ConfigValue =
     a => ConfigValueFactory.fromAnyRef(a)
 
-  implicit val durationToFiniteDuration: Duration => FiniteDuration =
+  implicit val javaDurationToFiniteDuration: JavaDuration => FiniteDuration =
     d => new FiniteDuration(d.toNanos, TimeUnit.NANOSECONDS)
 
-  implicit val finiteDurationToDuration: FiniteDuration => Duration =
-    f => f.asInstanceOf[Duration]
+  implicit val finiteDurationToJavaDuration: FiniteDuration => JavaDuration =
+    f => f.asInstanceOf[JavaDuration]
 
   implicit class ConfiguredFiniteDuration(path: String)(implicit config: Config) {
     def seconds = new FiniteDuration(config.getDuration(path, TimeUnit.SECONDS), TimeUnit.SECONDS)
+  }
+
+  implicit class ConfigOps(config: Config) {
+    def duration(path: String, default: Duration): Duration = {
+      val d = Try {
+        config.getDuration(path)
+      } getOrElse JavaDuration.ofNanos(default.toNanos)
+
+      d
+    }
+
+    def int(path: String, default: Int): Int = Try {
+      config.getInt(path)
+    } getOrElse default
   }
 }
