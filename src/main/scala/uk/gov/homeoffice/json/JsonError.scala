46947878b24bd69bf752d066b170be5efc0ac256
Benjamin Parker <benjamin@needstyping.com>
2015-08-03 11:40:58 +0100

    throwable as JSON in the JsonError

--- src/main/scala/uk/gov/homeoffice/json/JsonError.scala
+++ src/main/scala/uk/gov/homeoffice/json/JsonError.scala
@@ -2,5 +2,16 @@ package uk.gov.homeoffice.json
 
 import org.json4s.JValue
 import org.json4s.JsonAST.JNothing
+import org.json4s.JsonDSL._
 
-case class JsonError(json: JValue = JNothing, error: Option[String] = None, throwable: Option[Throwable] = None)
+case class JsonError(json: JValue = JNothing, error: Option[String] = None, throwable: Option[Throwable] = None) {
+  def throwableAsJson: Option[JValue] = throwable map { t =>
+    ("errorMessage" -> t.getMessage) ~
+    ("stackTrace" -> t.getStackTrace.toList.map { st =>
+      ("file" -> st.getFileName) ~
+      ("class" -> st.getClassName) ~
+      ("method" -> st.getMethodName) ~
+      ("line" -> st.getLineNumber)
+    })
+  }
+}
---
+++ src/test/scala/uk/gov/homeoffice/json/JsonErrorSpec.scala
@@ -0,0 +1,51 @@
+package uk.gov.homeoffice.json
+
+import org.specs2.mutable.Specification
+import org.json4s.native.JsonMethods._
+
+
+class JsonErrorSpec extends Specification {
+
+  "Exception" should {
+    "be converted to JSON" in {
+
+      val exception = new Exception("There was an error")
+      exception.setStackTrace(Array(
+        new StackTraceElement("class1", "method1", "file1", 23),
+        new StackTraceElement("class2", "method2", "file2", 165),
+        new StackTraceElement("class3", "method3", "file3", 12)
+      ))
+      val jsonError = JsonError(throwable = Some(exception))
+
+      val expectedJSON = parse( """
+          {
+           "errorMessage": "There was an error",
+           "stackTrace": [
+             {
+              "file": "file1"
+              "class": "class1"
+              "method": "method1"
+              "line": 23
+             },
+            {
+             "file": "file2"
+             "class": "class2"
+             "method": "method2"
+             "line": 165
+            },
+                         {
+             "file": "file3"
+             "class": "class3"
+             "method": "method3"
+             "line": 12
+            },
+           ]
+          }
+                                """
+      )
+
+      jsonError.throwableAsJson must beSome(expectedJSON)
+    }
+  }
+
+}
