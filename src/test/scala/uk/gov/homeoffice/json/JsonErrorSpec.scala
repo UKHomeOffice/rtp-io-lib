48c28ab09bb0fcb2cc42c5a0588a602dc021a15f
David Ainslie <dainslie@gmail.com>
2015-08-03 12:27:45 +0100

    Formatting

--- src/test/scala/uk/gov/homeoffice/json/JsonErrorSpec.scala
+++ src/test/scala/uk/gov/homeoffice/json/JsonErrorSpec.scala
@@ -3,7 +3,6 @@ package uk.gov.homeoffice.json
 import org.specs2.mutable.Specification
 import org.json4s.native.JsonMethods._
 
-
 class JsonErrorSpec extends Specification {
 
   "JsonError" should {
@@ -11,42 +10,43 @@ class JsonErrorSpec extends Specification {
     "be converted to JSON" in {
 
       val exception = new Exception("There was an error")
+
       exception.setStackTrace(Array(
         new StackTraceElement("class1", "method1", "file1", 23),
         new StackTraceElement("class2", "method2", "file2", 165),
         new StackTraceElement("class3", "method3", "file3", 12)
       ))
+
       val jsonError = JsonError(error = Some("Problem with my app"), throwable = Some(exception))
 
-      val expectedJSON = parse( """
+      val expectedJSON = parse("""
+      {
+       "error": "Problem with my app",
+       "errorStackTrace": {
+         "errorMessage": "There was an error",
+         "stackTrace": [
+           {
+            "file": "file1"
+            "class": "class1"
+            "method": "method1"
+            "line": 23
+           },
           {
-           "error": "Problem with my app",
-           "errorStackTrace": {
-             "errorMessage": "There was an error",
-             "stackTrace": [
-               {
-                "file": "file1"
-                "class": "class1"
-                "method": "method1"
-                "line": 23
-               },
-              {
-               "file": "file2"
-               "class": "class2"
-               "method": "method2"
-               "line": 165
-              },
-              {
-               "file": "file3"
-               "class": "class3"
-               "method": "method3"
-               "line": 12
-              },
-             ]
-           }
-          }
-                                """
-      )
+           "file": "file2"
+           "class": "class2"
+           "method": "method2"
+           "line": 165
+          },
+          {
+           "file": "file3"
+           "class": "class3"
+           "method": "method3"
+           "line": 12
+          },
+         ]
+       }
+      }
+      """)
 
       jsonError.asJson mustEqual expectedJSON
     }
