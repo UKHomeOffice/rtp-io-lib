ce8af218580bee0cbb73a9d2d1ea838747d18b5e
David Ainslie <dainslie@gmail.com>
2015-08-03 12:20:53 +0100

    Error stack trace in JSON

--- src/main/scala/uk/gov/homeoffice/json/JsonError.scala
+++ src/main/scala/uk/gov/homeoffice/json/JsonError.scala
@@ -1,10 +1,16 @@
 package uk.gov.homeoffice.json
 
 import org.json4s.JValue
-import org.json4s.JsonAST.JNothing
+import org.json4s.JsonAST.{JObject, JNothing}
 import org.json4s.JsonDSL._
 
 case class JsonError(json: JValue = JNothing, error: Option[String] = None, throwable: Option[Throwable] = None) {
+  def asJson = JObject() merge json merge {
+    error.fold(JObject()) { error => "error" -> error }
+  } merge {
+    throwableAsJson.fold(JObject()) { throwableJson => "errorStackTrace" -> throwableJson }
+  }
+
   def throwableAsJson: Option[JValue] = throwable map { t =>
     ("errorMessage" -> t.getMessage) ~
     ("stackTrace" -> t.getStackTrace.toList.map { st =>
--- src/test/scala/uk/gov/homeoffice/json/JsonErrorSpec.scala
+++ src/test/scala/uk/gov/homeoffice/json/JsonErrorSpec.scala
@@ -6,7 +6,7 @@ import org.json4s.native.JsonMethods._
 
 class JsonErrorSpec extends Specification {
 
-  "Exception" should {
+  "JsonError" should {
 
     "be converted to JSON" in {
 
@@ -16,37 +16,39 @@ class JsonErrorSpec extends Specification {
         new StackTraceElement("class2", "method2", "file2", 165),
         new StackTraceElement("class3", "method3", "file3", 12)
       ))
-      val jsonError = JsonError(throwable = Some(exception))
+      val jsonError = JsonError(error = Some("Problem with my app"), throwable = Some(exception))
 
       val expectedJSON = parse( """
           {
-           "errorMessage": "There was an error",
-           "stackTrace": [
-             {
-              "file": "file1"
-              "class": "class1"
-              "method": "method1"
-              "line": 23
-             },
-            {
-             "file": "file2"
-             "class": "class2"
-             "method": "method2"
-             "line": 165
-            },
-            {
-             "file": "file3"
-             "class": "class3"
-             "method": "method3"
-             "line": 12
-            },
-           ]
+           "error": "Problem with my app",
+           "errorStackTrace": {
+             "errorMessage": "There was an error",
+             "stackTrace": [
+               {
+                "file": "file1"
+                "class": "class1"
+                "method": "method1"
+                "line": 23
+               },
+              {
+               "file": "file2"
+               "class": "class2"
+               "method": "method2"
+               "line": 165
+              },
+              {
+               "file": "file3"
+               "class": "class3"
+               "method": "method3"
+               "line": 12
+              },
+             ]
+           }
           }
                                 """
       )
 
-      jsonError.throwableAsJson must beSome(expectedJSON)
+      jsonError.asJson mustEqual expectedJSON
     }
   }
-
 }
