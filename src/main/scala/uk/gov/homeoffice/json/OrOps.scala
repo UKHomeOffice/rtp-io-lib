6a0b74482f1b0be5c81363d2e8ece43b6844945c
David Ainslie <dainslie@gmail.com>
2015-08-10 13:54:15 +0100

    Avoid compiler warning

--- src/main/scala/uk/gov/homeoffice/json/OrOps.scala
+++ src/main/scala/uk/gov/homeoffice/json/OrOps.scala
@@ -4,14 +4,8 @@ import scala.reflect.ClassTag
 import org.scalactic.{Bad, Good, Or}
 
 trait OrOps {
-  /*def good[R : ClassTag, G, B]: G Or B => R = {
-    case Good(r: R) => r
-    case Bad(JsonError(_, error, Some(t))) => throw t
-    case Bad(e) => throw new Exception(s"Unexpected: $e")
-  }*/
-
-  def good[R, G, B]: G Or B => R = {
-    case Good(r: R) => r
+  def good[R, G, B](implicit ct: ClassTag[R]): G Or B => R = {
+    case Good(r) => r.asInstanceOf[R]
     case Bad(JsonError(_, error, Some(t))) => throw t
     case Bad(e) => throw new Exception(s"Unexpected: $e")
   }
