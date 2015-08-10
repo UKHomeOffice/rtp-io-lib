4f6c2aa1d67d11148235cc208b1e62bd3c16fd85
David Ainslie <dainslie@gmail.com>
2015-08-10 08:32:53 +0100

    Have to sort out use of ClassTag

--- src/main/scala/uk/gov/homeoffice/json/OrOps.scala
+++ src/main/scala/uk/gov/homeoffice/json/OrOps.scala
@@ -4,7 +4,13 @@ import scala.reflect.ClassTag
 import org.scalactic.{Bad, Good, Or}
 
 trait OrOps {
-  def good[R : ClassTag, G, B]: G Or B => R = {
+  /*def good[R : ClassTag, G, B]: G Or B => R = {
+    case Good(r: R) => r
+    case Bad(JsonError(_, error, Some(t))) => throw t
+    case Bad(e) => throw new Exception(s"Unexpected: $e")
+  }*/
+
+  def good[R, G, B]: G Or B => R = {
     case Good(r: R) => r
     case Bad(JsonError(_, error, Some(t))) => throw t
     case Bad(e) => throw new Exception(s"Unexpected: $e")
