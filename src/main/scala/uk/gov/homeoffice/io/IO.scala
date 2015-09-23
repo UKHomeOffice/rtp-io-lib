b7d4fcf1cc713080cabea7e1dbc6cbddbdd6baf3
Benjamin Parker <benjamin@needstyping.com>
2015-09-23 16:39:25 +0100

    Making functions more readable

--- src/main/scala/uk/gov/homeoffice/io/IO.scala
+++ src/main/scala/uk/gov/homeoffice/io/IO.scala
@@ -6,21 +6,18 @@ import scala.io.Source
 import scala.util.{Success, Failure, Try}
 
 trait IO {
-  val classpathResource: String => InputStream =
-    classpath => getClass.getResourceAsStream(classpath)
+  def classpathResource(classpath: String): InputStream = getClass.getResourceAsStream(classpath)
 
-  val fromClasspath: String => Try[URL] =
-    classpath => Try {
+  def fromClasspath(classpath: String): Try[URL] =
+    Try {
       getClass.getResource(classpath)
     } flatMap { url =>
       if (url == null) Failure(new IOException("Resource not found"))
       else Success(url)
     }
 
-  val urlContentToString: URL => Try[String] =
-    url => Try { Source.fromURL(url).getLines().mkString }
+  def urlContentToString(url: URL): Try[String] = Try { Source.fromURL(url).getLines().mkString }
 
   /** Makes sure that a given string representing a file path is operating system agnostic. */
-  val path: String => String =
-    p => p.replaceAll("""\\""", "/").replaceAll("/", File.separator)
+  def path(p: String): String = p.replaceAll("""\\""", "/").replaceAll("/", File.separator)
 }
