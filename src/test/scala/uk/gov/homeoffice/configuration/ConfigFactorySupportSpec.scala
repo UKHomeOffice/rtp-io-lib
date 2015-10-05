ee619621040cf2d5e610542dc04ad6e16afa0c70
David Ainslie <dainslie@gmail.com>
2015-10-05 17:29:02 +0100

    Refactored example

--- src/test/scala/uk/gov/homeoffice/configuration/ConfigFactorySupportSpec.scala
+++ src/test/scala/uk/gov/homeoffice/configuration/ConfigFactorySupportSpec.scala
@@ -2,11 +2,14 @@ package uk.gov.homeoffice.configuration
 
 import java.time.Duration
 import scala.concurrent.duration.FiniteDuration
+import org.specs2.matcher.Scope
 import org.specs2.mutable.Specification
 
-class ConfigFactorySupportSpec extends Specification with ConfigFactorySupport {
+class ConfigFactorySupportSpec extends Specification {
+  trait Context extends Scope with ConfigFactorySupport
+
   "Configuration support" should {
-    "convert a 5 minute duration to a finite duration" in {
+    "convert a 5 minute duration to a finite duration" in new Context {
       val finitieDuration: FiniteDuration = Duration.parse("PT5M")
       finitieDuration.toMinutes mustEqual 5
     }
