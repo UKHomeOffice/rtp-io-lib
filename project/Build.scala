4a08e30b794e06e3cceaf747b31e5cf85a736374
David Ainslie <dainslie@gmail.com>
2016-02-26 17:50:59 +0000

    Cleaned up build

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import sbt._
 object Build extends Build {
   val moduleName = "rtp-io-lib"
 
-  lazy val io = Project(id = moduleName, base = file("."))
+  val root = Project(id = moduleName, base = file("."))
     .configs(IntegrationTest)
     .settings(Defaults.itSettings: _*)
     .settings(
@@ -27,43 +27,29 @@ object Build extends Build {
         "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
         "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
         "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases",
-        "Kamon Repository" at "http://repo.kamon.io"),
-      libraryDependencies ++= Seq(
+        "Kamon Repository" at "http://repo.kamon.io"
+      )
+    )
+    .settings(libraryDependencies ++= {
+      val `json4s-version` = "3.2.11"
+      val `logback-version` = "1.1.3"
+      val `rtp-test-lib-version` = "1.2.0-SNAPSHOT"
+
+      Seq(
         "com.typesafe" % "config" % "1.3.0" withSources(),
-        "org.json4s" %% "json4s-native" % "3.2.11" withSources(),
-        "org.json4s" %% "json4s-jackson" % "3.2.11" withSources(),
-        "org.json4s" %% "json4s-ext" % "3.2.11" withSources(),
-        "org.json4s" %% "json4s-mongo" % "3.2.11" withSources(),
+        "org.json4s" %% "json4s-native" % `json4s-version` withSources(),
+        "org.json4s" %% "json4s-jackson" % `json4s-version` withSources(),
+        "org.json4s" %% "json4s-ext" % `json4s-version` withSources(),
+        "org.json4s" %% "json4s-mongo" % `json4s-version` withSources(),
         "com.github.fge" % "json-schema-validator" % "2.2.6" withSources(),
         "org.scala-lang.modules" %% "scala-pickling" % "0.10.1" withSources(),
         "com.lihaoyi" %% "pprint" % "0.3.4",
         "org.clapper" %% "grizzled-slf4j" % "1.0.2",
-        "ch.qos.logback" % "logback-core" % "1.1.3",
-        "ch.qos.logback" % "logback-classic" % "1.1.3"),
-      libraryDependencies ++= Seq()
-    )
-
-  val testPath = "../rtp-test-lib"
-
-  def existsLocallyAndNotOnJenkins(filePath: String) = file(filePath).exists && !file(filePath + "/nextBuildNumber").exists()
-
-  val root = if ((existsLocallyAndNotOnJenkins(testPath) || existsLocallyAndNotOnJenkins("../" + testPath)) && sys.props.get("jenkins").isEmpty) {
-    println("================")
-    println("Build locally io")
-    println("================")
-
-    val testLib = ProjectRef(file(testPath), "rtp-test-lib")
-    io.dependsOn(testLib % "test->test;compile->compile")
-
-  } else {
-    println("===================")
-    println("Build on Jenkins io")
-    println("===================")
-
-    io.settings(
-      libraryDependencies ++= Seq(
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.0" withSources(),
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.0" % Test classifier "tests" withSources()
-      ))
-  }
+        "ch.qos.logback" % "logback-core" % `logback-version`,
+        "ch.qos.logback" % "logback-classic" % `logback-version`,
+        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.0" withSources()
+      ) ++ Seq(
+        "uk.gov.homeoffice" %% "rtp-test-lib" % `rtp-test-lib-version` % Test classifier "tests" withSources()
+      )
+    })
 }
