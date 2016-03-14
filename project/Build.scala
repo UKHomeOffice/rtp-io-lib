22b08006c0f4112a0eeada58bd44df07db98253b
David Ainslie <dainslie@gmail.com>
2016-03-14 11:12:01 +0000

    Version 1.6.0-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.5.0",
+      version := "1.6.0-SNAPSHOT",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
