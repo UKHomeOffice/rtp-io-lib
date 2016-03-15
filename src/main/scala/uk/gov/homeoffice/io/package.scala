feaf1e9023120d59438e98dff62ea98b39effe51
David Ainslie <dainslie@gmail.com>
2016-03-15 15:33:34 +0000

    Simplified implicit to instantiate a Resource

--- src/main/scala/uk/gov/homeoffice/io/package.scala
+++ src/main/scala/uk/gov/homeoffice/io/package.scala
@@ -1,12 +1,5 @@
 package uk.gov.homeoffice
 
-import java.io.File
-import java.net.URL
-
 package object io {
-  implicit val file2Resource: File => Resource[File] = toResource
-
-  implicit val url2Resource: URL => Resource[URL] = toResource
-
-  private def toResource[R](r: R) = Resource(r)
+  implicit def toResource[R](r: R): Resource[R] = Resource(r)
 }
