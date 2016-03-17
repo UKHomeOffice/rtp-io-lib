37145c70731b1e6c8a19488b30496f08a24cecfd
David Ainslie <dainslie@gmail.com>
2016-03-17 14:50:36 +0000

    Bumped version to 1.7.0

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.6.0",
+      version := "1.7.0",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
