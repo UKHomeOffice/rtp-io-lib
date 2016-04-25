109e0dbe485a76d6f68f71e033d0f8309f3c8d8e
David Ainslie <dainslie@gmail.com>
2016-04-25 11:04:59 +0100

    Can complete result of Promise with or without a Future.

--- src/test/scala/uk/gov/homeoffice/concurrent/PromiseOpsSpec.scala
+++ src/test/scala/uk/gov/homeoffice/concurrent/PromiseOpsSpec.scala
@@ -10,12 +10,28 @@ class PromiseOpsSpec(implicit ev: ExecutionEnv) extends Specification with Promi
     "be realized and still give back the result" in {
       val promise = Promise[String]()
 
+      promise <~ "Well done!"
+
+      promise.future must beEqualTo("Well done!").await
+    }
+
+    "be realized and still give back the result" in {
+      val promise = Promise[String]()
+
+      promise <~ "Well done!"
+
+      promise.future must beEqualTo("Well done!").await
+    }
+
+    "be realized with a future and still give back the result" in {
+      val promise = Promise[String]()
+
       promise <~ Future { "Well done!" }
 
       promise.future must beEqualTo("Well done!").await
     }
 
-    """be realized (using named "realized") and still give back the result""" in {
+    """be realized with a future (using named "realized") and still give back the result""" in {
       val promise = Promise[String]()
 
       promise realized Future { "Well done!" }
@@ -23,7 +39,7 @@ class PromiseOpsSpec(implicit ev: ExecutionEnv) extends Specification with Promi
       promise.future must beEqualTo("Well done!").await
     }
 
-    "be not be realized because of an exception" in {
+    "be not be realized with a future because of an exception" in {
       val promise = Promise[String]()
 
       promise <~> Future { throw new Exception("Whoops!") }
