36c85141a7ac08b22bacab7d311ec435540eea23
Ryan Da Costa <Ryan.daCosta@digital.homeoffice.gov.uk>
2016-06-30 10:10:01 +0100

    [NO-JIRA]: Move crypto to rtp-io-lib

---
+++ src/main/scala/uk/gov/homeoffice/crypt/Crypto.scala
@@ -0,0 +1,51 @@
+package uk.gov.homeoffice.crypt
+
+import java.nio.charset.StandardCharsets._
+import javax.crypto.Cipher
+import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
+import scala.util.Try
+import org.apache.commons.codec.binary.Base64._
+import org.json4s.JValue
+import org.json4s.JsonDSL._
+import uk.gov.homeoffice.json.JsonFormats
+
+case class Secrets(encryptionKey: String, signingPassword: String)
+
+trait Crypto extends JsonFormats {
+  def encrypt(data: String, iv: String)(implicit secrets: Secrets): JValue = {
+    val cipher: Cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+    val secretKey = new SecretKeySpec(secrets.encryptionKey.getBytes(UTF_8), "AES")
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(iv.getBytes(UTF_8), 0, cipher.getBlockSize))
+    val encryptedData = cipher.doFinal(data.getBytes(UTF_8))
+
+    ("data" -> sign(encodeBase64String(encryptedData), secrets.signingPassword)) ~ ("iv" -> sign(encodeBase64String(iv.getBytes()), secrets.signingPassword))
+  }
+
+  def decrypt(j: JValue)(implicit secrets: Secrets): Try[String] = Try {
+    val signedData = (j \ "data").extract[String]
+    val signedIV = (j \ "iv").extract[String]
+
+    if (verifySignatureFor(signedData, secrets.signingPassword) && verifySignatureFor(signedIV, secrets.signingPassword)) {
+      val key = new SecretKeySpec(secrets.encryptionKey.getBytes(UTF_8), "AES")
+      val cipher: Cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+
+      cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(decodeBase64(signedIV.getBytes()), 0, cipher.getBlockSize))
+      new String(cipher.doFinal(decodeBase64(signedData.getBytes(UTF_8))))
+    } else {
+      throw new IllegalAccessException(s"Badly signed data $signedData & signedIV $signedIV")
+    }
+  }
+
+  private def verifySignatureFor(signedData: String, signingPassword: String): Boolean = {
+    require(signedData.nonEmpty)
+    val signedDataWithClientPassword = sign(signedData.split("----").head, signingPassword)
+
+    if (signedData == signedDataWithClientPassword) true else false
+  }
+
+  private def sign(data: String, signingPassword: String): String = {
+    val hmac256 = new HmacSha256
+    val signature = encodeBase64String(hmac256.create(signingPassword, data))
+    s"$data----$signature"
+  }
+}
---
+++ src/test/scala/uk/gov/homeoffice/crypt/CryptoSpec.scala
@@ -0,0 +1,73 @@
+package uk.gov.homeoffice.crypt
+
+import org.json4s.JsonAST.JObject
+import org.json4s.jackson.JsonMethods._
+import org.json4s.{JString, JValue}
+import org.specs2.matcher.Scope
+import org.specs2.mutable.Specification
+
+import scala.util.{Random, Success}
+
+class CryptoSpec extends Specification {
+
+  trait Context extends Crypto with Scope {
+    val encryptionIV = "abcdadsgdighcige"
+    implicit val secrets = Secrets("4a0783fc-2ca1-11e6-b67b-9e71128c", "A0Suns5tMarked5D1stinct7on")
+  }
+
+  "data" should {
+    val rawData = "test"
+
+    "be encrypted and signed" in new Context {
+      encrypt(rawData, encryptionIV) must beLike {
+        case j: JValue =>
+          j \ "data" must beAnInstanceOf[JString]
+          j \ "iv" must beAnInstanceOf[JString]
+      }
+    }
+
+    "be decrypted with given signing password" in new Context {
+      val rawData = "test"
+      val r = Random
+      decrypt(encrypt(rawData, r.nextString(16))) must beLike {
+        case Success(decryptedData) => decryptedData mustEqual rawData
+      }
+    }
+
+    "fail to be decrypted and empty json data" in new Context {
+      decrypt(JObject()) must beFailedTry
+    }
+
+    "successfully decrypt the signed data passed as Json" in new Context {
+      val signedJSON = parse(
+        """{
+          |"data" : "JUxPd7WN3xfPnaIwctMcPQ==----CqKlGQeY16Pp6HOBSdQF+Fw2SodpoKq2tnTn6WpAreg=",
+          | "iv" : "5rm654u0xJ7ir4foo6frgZzll7vmgJvmiKbqhbXovK7ij4vli7fpqqTkiYXqq6g=----aLfxZ02vLn1xO1IwAd7hewkbS2YVcDMasplz4WaUJHY="
+          | }""".stripMargin)
+
+      decrypt(signedJSON) must beLike {
+        case Success(data) => data mustEqual rawData
+      }
+    }
+
+    "fail to decrypt the badly signed data passed as Json" in new Context {
+      val signedJSON = parse(
+        """{
+          |"data" : "xGarbled5sXKTdBVO3BSdYkA==----UoRZUwpLDYdUYA6U4tqsYtZqNqR5MZeM6Oau68ZYSd8=",
+          | "iv" : "6IGK7Zau5oS94Zit5qSH7JG74aOU6J6F5KKY7LyP6rC957Cv4pKI4rOW65q04oWR----wMHiQwXjC9ecsYRNVf0shiJYw9uvYZjBxpExWR31yks="
+          | }""".stripMargin)
+
+      decrypt(signedJSON) must beFailedTry.like {
+        case e: IllegalAccessException => e.getMessage must startWith("Badly signed data")
+      }
+    }
+
+    "decode with base64 decoder" in new Context {
+      val decryptedData = decrypt(encrypt(rawData, encryptionIV))
+      decryptedData must beLike {
+        case Success(data) => data mustEqual rawData
+      }
+    }
+  }
+
+}
