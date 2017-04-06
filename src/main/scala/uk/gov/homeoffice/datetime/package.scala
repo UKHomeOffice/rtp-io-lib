fb25f58deb01a55eb42072636a1040eb70a6bd1d
jackmaloney <jack.maloney@transformuk.com>
2017-04-06 12:17:28 +0100

    Add date time formatter

--- src/main/scala/uk/gov/homeoffice/datetime/package.scala
+++ src/main/scala/uk/gov/homeoffice/datetime/package.scala
@@ -1,12 +1,14 @@
 package uk.gov.homeoffice
 
 import com.github.nscala_time.time.Imports._
-import org.joda.time.format.DateTimeFormatter
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
 
 package object datetime {
   val `yyyy-MM-dd` = DateTimeFormat forPattern "yyyy-MM-dd"
 
   val `yyyy-dd-MM` = DateTimeFormat forPattern "yyyy-dd-MM"
+
+  val `yyyy-MM-dd HH:mm:ss` = DateTimeFormat forPattern "yyyy-MM-dd HH:mm:ss"
 
   implicit class StringWithDateOps(s: String) {
     def as(formatter: DateTimeFormatter): DateTime = formatter parseDateTime s
--- src/test/scala/uk/gov/homeoffice/datetime/DateTimeSpec.scala
+++ src/test/scala/uk/gov/homeoffice/datetime/DateTimeSpec.scala
@@ -14,4 +14,18 @@ class DateTimeSpec extends Specification {
       }
     }
   }
+
+  "Date time as a String" should {
+    "be converted to DateTime formatted as yyyy-MM-dd HH:mm:ss" in {
+      "1999-12-30 10:30:32" as `yyyy-MM-dd HH:mm:ss` must beLike {
+        case dt: DateTime =>
+          dt.getYear mustEqual 1999
+          dt.getMonthOfYear mustEqual 12
+          dt.getDayOfMonth mustEqual 30
+          dt.getHourOfDay mustEqual 10
+          dt.getMinuteOfHour mustEqual 30
+          dt.getSecondOfMinute mustEqual 32
+      }
+    }
+  }
 }
